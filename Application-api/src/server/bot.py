245c586a9e0f7c7ef7d933b7a73992977652d63e
Ovsienko023 <Ovsienko023@gmail.com>
2020-04-27 20:08:46 +0300

    mod bot

--- Application-api/src/server/bot.py
+++ Application-api/src/server/bot.py
@@ -7,13 +7,18 @@ def main_bot(request):
     print(request.json)
     bot = TeleWrapper(request)
     # if bot.is_start():
-    bot.send_message()
+    
+    # bot.send_message('Hi')
+    bot.inline_keyboard()
     # print(request.headers)
 
 
 class TeleWrapper:
     def __init__(self, request):
         self.request = request
+        self.token = r"1026862035:AAG-mBQD7TgE_yaiE3uC38-W-Q5KRQ6uy1I"
+        self.method = "sendMessage"
+        self.url = f"https://api.telegram.org/bot{self.token}/{self.method}"
 
     def is_start(self):
         messege = self.request.json['message']['text']
@@ -27,39 +32,33 @@ class TeleWrapper:
             send_message = f'{first_name}, для начала работы введите "start", для помощи "help"'
             self.send_message(chat_id, send_message)
 
-    # def send_message(self, chat_id, messege):
-    #     method = "sendMessage"
-    #     token = r"1026862035:AAG-mBQD7TgE_yaiE3uC38-W-Q5KRQ6uy1I"
-    #     url = f"https://api.telegram.org/bot{token}/{method}"
-    #     data = {"chat_id": chat_id, "text": messege}
-    #     requests.post(url, data=data)
-    #### кнопка со ссылкой
-    # def send_message(self):
-    #     chat_id = self.request.json['message']['chat']['id']
-    #     method = "sendMessage"
-    #     token = r"1026862035:AAG-mBQD7TgE_yaiE3uC38-W-Q5KRQ6uy1I"
-    #     url = f"https://api.telegram.org/bot{token}/{method}"
-    #     message_id = self.request.json["message"]["message_id"]
-
-    #     reply = json.dumps({"inline_keyboard":[[{"text":"текст1","url":"http://ya.ru"}]]})
-    #     params = {"chat_id": chat_id, "text": "!!!", "reply_markup":reply}
-    #     requests.post(url, params)
-    #### Кнопка которая пишет текст
-    def send_message(self):
+    def send_message(self, messege):
         chat_id = self.request.json['message']['chat']['id']
-        method = "sendMessage"
-        token = r"1026862035:AAG-mBQD7TgE_yaiE3uC38-W-Q5KRQ6uy1I"
-        url = f"https://api.telegram.org/bot{token}/{method}"
+        data = {"chat_id": chat_id, "text": messege}
+        requests.post(self.url, data=data)
+    
+    #### кнопка со ссылкой
+    def inline_keyboard(self):
+        """ Link button """
+
+        chat_id = self.request.json['message']['chat']['id']
+        message_id = self.request.json["message"]["message_id"]
+
+        reply = json.dumps({"inline_keyboard":[[{"text":"Ниги","url":"https://memepedia.ru/negry-s-grobom/"}]]})
+        params = {"chat_id": chat_id, "text": "!!!", "reply_markup":reply}
+        requests.post(self.url, params)
+    
+    #### Кнопка которая пишет текст
+    def keyboard(self):
+        """ Button """
+        
+        chat_id = self.request.json['message']['chat']['id']
         message_id = self.request.json["message"]["message_id"]
 
         reply = json.dumps({"keyboard":[[{"text":"текст1"}]]})
         params = {"chat_id": chat_id, "text": "!!!", "reply_markup": reply}
-        requests.post(url, params)
+        requests.post(self.url, params)
 
-        # requests.post(url, data=data)
-# {'inline_keyboard':[[url_button, question_button],[switch_button]]}
-    # def command(self):
-    #     pass
 
 
 
